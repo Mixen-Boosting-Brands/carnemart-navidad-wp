3d52a1fce8d23d1cd1f028ba68f86ac78f82e68b
Luis Carlos Pando <hey@luiscarlospando.com>
2025-09-09 13:48:03 -0600

    Armar templates page y single

--- template-parts/content-page-single.php
+++ template-parts/content-page-single.php
@@ -18,16 +18,18 @@
         <?php endif; ?>>
     </section>
 
-    <section class="container py-60">
-        <div class="row">
-            <div class="col-md-12">
-                <h1 data-aos="fade-up"
-                data-aos-duration="1000"
-                data-aos-delay="100"><?php the_title(); ?></h1>
-                <div class="content" data-aos="fade-up"
-                data-aos-duration="1000"
-                data-aos-delay="200">
-                    <?php the_content(); ?>
+    <section class="py-60">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1 data-aos="fade-up"
+                    data-aos-duration="1000"
+                    data-aos-delay="100"><?php the_title(); ?></h1>
+                    <div class="content" data-aos="fade-up"
+                    data-aos-duration="1000"
+                    data-aos-delay="200">
+                        <?php the_content(); ?>
+                    </div>
                 </div>
             </div>
         </div>
