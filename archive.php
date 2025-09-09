cb1f678d2e3715dd71551c5a3c388db2d5622858
Luis Carlos Pando <hey@luiscarlospando.com>
2025-09-09 14:24:23 -0600

    Armar templates page y single

--- archive.php
+++ archive.php
@@ -3,14 +3,6 @@
 <main id="primary" class="site-main">
 
     <?php if (have_posts()): ?>
-        <section class="py-60">
-            <div class="container mb-5 text-center">
-                <h1 class="mb-3"><?php the_archive_title(); ?></h1>
-                <?php if (the_archive_description()): ?>
-                    <p class="lead"><?php the_archive_description(); ?></p>
-                <?php endif; ?>
-            </div>
-        </section>
 
         <?php get_template_part("template-parts/content", "blog"); ?>
 
--- category.php
+++ category.php
@@ -3,14 +3,6 @@
 <main id="primary" class="site-main">
 
     <?php if (have_posts()): ?>
-        <section class="py-60">
-            <div class="container mb-5 text-center">
-                <h1 class="mb-3"><?php single_cat_title(); ?></h1>
-                <?php if (category_description()): ?>
-                    <p class="lead"><?php echo category_description(); ?></p>
-                <?php endif; ?>
-            </div>
-        </section>
 
         <?php get_template_part("template-parts/content", "blog"); ?>
 
